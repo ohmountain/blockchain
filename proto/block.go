840dd24c5488cfc18fc1f7046866ffeec76c88a5
renshan <1005110700@qq.com>
2017-12-22 12:49:59 +0800

    Add serialize and deserialize

--- proto/block.go
+++ proto/block.go
@@ -1,6 +1,8 @@
 package proto
 
 import (
+	"bytes"
+	"encoding/gob"
 	"encoding/hex"
 	"time"
 )
@@ -35,6 +37,29 @@ func (b *Block) GetHashString() string {
 
 func (b *Block) Validate() bool {
 	return b.PoW.Validate()
+}
+
+func (b *Block) Serialize() ([]byte, error) {
+	var result bytes.Buffer
+
+	encoder := gob.NewEncoder(&result)
+	if err := encoder.Encode(b); err != nil {
+		return nil, err
+	}
+
+	return result.Bytes(), nil
+}
+
+func Deserialize(b []byte) (*Block, error) {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(b))
+
+	if err := decoder.Decode(&block); err != nil {
+		return nil, err
+	}
+
+	return &block, nil
 }
 
 func CreateBlock(data []byte) *Block {
